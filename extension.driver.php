8ec477aadbaef91c8927d37b36d30bc67d7c07be
tjort <jordi@verdultworks.nl>
2015-02-05 18:08:14 +0100

    Update extension.driver.php
    Bugfix. CSS didn't load.

--- extension.driver.php
+++ extension.driver.php
@@ -4,7 +4,7 @@
 
 		public function about() {
 			return array(
-				'name'			=> 'Slick: an even better looking Symphony',
+				'name'			=> 'Slick: a better looking Symphony',
 				'version'		=> '1.0',
 				'release-date'	=> '2015-01-28',
 				'author'		=> array(
@@ -16,6 +16,7 @@
 		}
 
 		public function getSubscribedDelegates() {
+
 			return array(
 				array(
 					'page' => '/backend/',
@@ -26,9 +27,11 @@
 		}
 
 		public function appendAssets($context) {
-			$page = $context['parent']->Page;
 
-			if ($page instanceof HTMLPage) {
+			if (class_exists('Administration')
+			&& Administration::instance() instanceof Administration
+			&& Administration::instance()->Page instanceof HTMLPage) {
+				$page =Administration::instance()->Page;
 				$page->addStylesheetToHead(URL . '/extensions/slick/assets/slick.css', 'screen', 1982);
 			}
 		}
